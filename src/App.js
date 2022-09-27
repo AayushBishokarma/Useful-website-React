d6619287c4164cfa3190b6a62ca2f8b3f420a8cb
Aayush <aayushsunar87@gmail.com>
2022-09-27 23:37:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Button from "./Button";
+const App= ()=> {
+    return <div>
+      <h1>Useful websites in single page.</h1>
+      <h3>Browse any website you want.</h3>
+     <Button name="Google" link="https://google.com"/>
+     <Button name="Facebook" link="https://facebook.com"/>
+     <Button name="Youtube" link="https://youtube.com"/>
+     <br/>
+     <Button name="LinkedIn" link="https://linkedin.com"/>
+     <Button name="Instagram" link="https://instagram.com"/>
+     <Button name="Github" link="https://github.com"/>
+     <br/>
+     <Button name="Messenger" link="https://messenger.com"/>
+     <Button name="Whatsapp" link="https://whatsapp.com"/>
+     <Button name="Skype" link="https://skype.com"/>
+
+    </div>
+  }
+  export default App;
